fd6d4f1bb98b89d6c33d72e4f2c8cad4fb3e4268
FabriAtom <fabrizioromano20@gmail.com>
2022-09-08 17:08:50 +0200

    push-erorr

--- src/components/ListCharactersComponent.vue
+++ src/components/ListCharactersComponent.vue
@@ -1,20 +1,11 @@
 <template>
-<div>
-  <b-card
-    title="Card Title"
-    img-src="https://picsum.photos/600/300/?image=25"
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 20rem;"
-    class="mb-2"
-  >
-    <b-card-text>
-      Some quick example text to build on the card title and make up the bulk of the card's content.
-    </b-card-text>
-
-    <b-button href="#" variant="primary">Go somewhere</b-button>
-  </b-card>
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
 </div>
 </template>
 
